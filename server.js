e6ec393e5a3670cf63305bb1b8a778e302d4bcc2
Anastasha Rachel Gunawan <rachelgunawan1999@gmail.com>
2026-02-18 00:27:27 -0800

    update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Start the server
 app.listen(PORT, () => {
